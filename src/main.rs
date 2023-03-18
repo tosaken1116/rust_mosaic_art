2375e5cab9a4c6cb74111303a02c951ab9b78a7b
tosaken1116 <nagato20021122@gmail.com>
2023-03-18 20:33:37 +0900

    add:エラーハンドリングの追加

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,117 @@
-use image::GenericImage;
-use image::{DynamicImage, GenericImageView,ImageBuffer, imageops::resize,Rgb,Rgba, Pixel};
-use std::u8;
-use std::{fs};
 use crate::fs::File;
+use core::panic;
+use image::GenericImage;
+use image::{imageops::resize, DynamicImage, GenericImageView, ImageBuffer, Pixel, Rgb, Rgba};
 use serde_json;
-use std::collections::HashMap;
 use serde_json::Result;
-use std::io::Write;
+use std::collections::HashMap;
+use std::fs;
 use std::io::Read;
-use std::io::BufReader;
+use std::io::Write;
+use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
-use std::sync::{ Arc};
+use std::u8;
 
 fn main() {
     let seeds_images_dir = "./src/seed_images";
-    // crop_images((&seeds_images_dir).to_string());
-    // save_img_colors((&seeds_images_dir).to_string());
+    crop_images((&seeds_images_dir).to_string());
+    save_img_colors((&seeds_images_dir).to_string());
     make_mosaic_art();
 }
 
+fn crop_images(dir_name: String) {
+    let files = match fs::read_dir(dir_name) {
+        Ok(f) => f,
+        Err(_) => panic!("Error reading directory"),
+    };
 
-fn crop_images(dir_name:String){
-    let files = fs::read_dir(dir_name).unwrap();
-
-    for (index,file) in files.enumerate(){
+    for (index, file) in files.enumerate() {
         let file = file.unwrap();
         let file_path = file.path();
-        let img = image::open(file_path).unwrap();
+        let img = match image::open(file_path.clone()) {
+            Ok(f) => f,
+            Err(_) => {
+                println!("can't open {:?}", file_path);
+                continue;
+            }
+        };
 
-        save_img(image::DynamicImage::ImageRgba8(resize_img(crop_img(img),50)),format!("./src/crop/{}.png",index.to_string()));
+        save_img(
+            image::DynamicImage::ImageRgba8(resize_img(crop_img(img), 50)),
+            format!("./src/crop/{}.png", index.to_string()),
+        );
     }
 }
 
-
-fn save_img_colors(dir_name:String){
-    let files = fs::read_dir(dir_name).unwrap();
+fn save_img_colors(dir_name: String) {
+    let files = match fs::read_dir(dir_name) {
+        Ok(f) => f,
+        Err(e) => panic!("can't read directory on save_img_colors {}",e),
+    };
     let mut color_code_dict = HashMap::new();
-    for (index,file) in files.enumerate(){
+    for (index, file) in files.enumerate() {
         let file_dir = file.unwrap();
         let file_path = file_dir.path();
-        let file = File::open(file_path).unwrap();
-        let mut buf_reader = BufReader::new(file);
-        let img =  image::load(&mut buf_reader, image::ImageFormat::Jpeg).unwrap();
-        // let img = image::open(file_path).unwrap();
-        color_code_dict.insert( index.to_string(),get_color_code( image::DynamicImage::ImageRgba8(resize_img(img,1))));
-
+        let img = match image::open(file_path.clone()) {
+            Ok(f) => f,
+            Err(_) => {
+                println!("can't open {:?}", file_path);
+                continue;
+            }
+        };
+        color_code_dict.insert(
+            index.to_string(),
+            get_color_code(image::DynamicImage::ImageRgba8(resize_img(img, 1))),
+        );
     }
-    save_color_code(&color_code_dict).unwrap();
+    match save_color_code(&color_code_dict) {
+        Err(_) => panic!("error was occurred while saving color code"),
+        Ok(_) => {}
+    };
 }
 
-fn crop_img(mut img:DynamicImage)->DynamicImage{
-
-    // 画像の幅と高さを取得
+fn crop_img(mut img: DynamicImage) -> DynamicImage {
     let (width, height) = img.dimensions();
 
-    // 切り取るサイズを計算
     let size = std::cmp::min(width, height);
 
-    // 正方形に切り取る
-    let cropped_img = img.crop(width/2-size/2, 0, size, size);
-    return cropped_img
+    let cropped_img = img.crop(width / 2 - size / 2, 0, size, size);
+    return cropped_img;
 }
 
-fn resize_img(img:DynamicImage,resize_num:u32)->ImageBuffer<Rgba<u8>, Vec<u8>>{
-    let resized_img = resize(&img, resize_num, resize_num, image::imageops::FilterType::Triangle);
+fn resize_img(img: DynamicImage, resize_num: u32) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let resized_img = resize(
+        &img,
+        resize_num,
+        resize_num,
+        image::imageops::FilterType::Triangle,
+    );
     resized_img
 }
 
-fn save_img(img:DynamicImage, save_path:String){
-    img.save(save_path).unwrap();
+fn save_img(img: DynamicImage, save_path: String) {
+    match img.save(save_path.clone()) {
+        Ok(_) => {}
+        Err(err) => println!("cannot save {}: {}", save_path, err),
+    }
 }
 
-fn get_color_code(img:DynamicImage)-> String {
+fn get_color_code(img: DynamicImage) -> String {
     let pixel: Rgb<u8> = img.get_pixel(0, 0).to_rgb();
-    return rgb_to_hex_string(&pixel)
+    return rgb_to_hex_string(&pixel);
 }
 
-fn save_color_code(color_codes: &HashMap<String,String>)->Result<()>{
+fn save_color_code(color_codes: &HashMap<String, String>) -> Result<()> {
     let serialized = serde_json::to_string(color_codes)?;
     let mut file = match File::create("color_code.json") {
         Ok(f) => f,
-        Err(e) => panic!("Error creating color")
+        Err(_) => panic!("Error creating color"),
     };
-    file.write_all(serialized.as_bytes()).unwrap();
+    match file.write_all(serialized.as_bytes()) {
+        Ok(_) => {}
+        Err(_) => panic!("Error writing color code"),
+    }
     Ok(())
 }
 
@@ -100,85 +128,133 @@ fn hex_to_rgb(hex: &str) -> [u8; 3] {
     [r, g, b]
 }
 
-fn calculate_color_distance(color1: [u8;3], color2: Rgb<u8>) -> i32{
-    (color1[0]as i32 - color2[0] as i32).abs().pow(2)+(color1[1]as i32 - color2[1] as i32).abs().pow(2) + (color1[2]as i32 - color2[2] as i32).abs().pow(2)
+fn calculate_color_distance(color1: [u8; 3], color2: Rgb<u8>) -> i32 {
+    (color1[0] as i32 - color2[0] as i32).abs().pow(2)
+        + (color1[1] as i32 - color2[1] as i32).abs().pow(2)
+        + (color1[2] as i32 - color2[2] as i32).abs().pow(2)
 }
 
-fn make_mosaic_image_row(mosaic_img:DynamicImage,width:u32,height:u32,index:u32,color_code_json:HashMap<String, String>)->ImageBuffer<Rgba<u8>, Vec<u8>>{
-    let mut result_mosaic_img:ImageBuffer<Rgba<u8>, Vec<u8>>=ImageBuffer::new(width*50, height/2*50);
-for y in 0..height/2{
-    for x in 0..width{
-            let pixel = mosaic_img.get_pixel(x, y+height/2*(index)).to_rgb();
-            let combine_image= image::open(format!("./src/crop/{}.png", calculate_min_color_distance_code(pixel,&color_code_json))).unwrap();
-            match result_mosaic_img.copy_from(&combine_image, x*50, y*50){
-                Ok(file)=>file,
-                Err(err)=>panic!("{}",err)
+fn make_mosaic_image_row(
+    mosaic_img: DynamicImage,
+    width: u32,
+    height: u32,
+    index: u32,
+    color_code_json: HashMap<String, String>,
+) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let mut result_mosaic_img: ImageBuffer<Rgba<u8>, Vec<u8>> =
+        ImageBuffer::new(width * 50, height / 2 * 50);
+    for y in 0..height / 2 {
+        for x in 0..width {
+            let pixel = mosaic_img.get_pixel(x, y + height / 2 * (index)).to_rgb();
+            let file_path = format!(
+                "./src/crop/{}.png",
+                calculate_min_color_distance_code(pixel, &color_code_json)
+            );
+            let combine_image = match image::open(file_path.clone()) {
+                Ok(f) => f,
+                Err(_) => {
+                    println!("cannot open image {}", file_path);
+                    continue;
+                }
+            };
+            match result_mosaic_img.copy_from(&combine_image, x * 50, y * 50) {
+                Ok(file) => file,
+                Err(err) => panic!("{}", err),
             }
         }
     }
     result_mosaic_img
 }
 
-fn make_mosaic_art(){
-    let mosaic_img = image::open("./src/source/seed.jpg").unwrap();
+fn make_mosaic_art() {
+    let mosaic_img = match image::open("./src/source/seed.jpg") {
+        Ok(f) => f,
+        Err(e) => panic!("cannot open mosaic_image {}", e),
+    };
     let (width, height) = mosaic_img.dimensions();
     let color_code_json = load_color_code_json();
 
     let mut handles = vec![];
-    let results = Arc::new(Mutex::new(Vec::new())); // 結果を格納する可変のVecをMutexで保護するArcを作成する
-    for i in 0..2{
-        let results = results.clone(); // Arcのクローンを作成し、スレッドに渡す
+    let results = Arc::new(Mutex::new(Vec::new()));
+    for i in 0..2 {
+        let results = results.clone();
         let mosaic_img = mosaic_img.clone();
         let color_code_json = color_code_json.clone();
         let handle = thread::spawn(move || {
-            let result = make_mosaic_image_row(mosaic_img,width.clone(),height.clone(),i,color_code_json);
-
-            let mut results = results.lock().unwrap();
-            results.push((i, result)); // 結果をMutexで保護された可変のVecに追加する
+            let result = make_mosaic_image_row(
+                mosaic_img,
+                width.clone(),
+                height.clone(),
+                i,
+                color_code_json,
+            );
+            let mut results = match results.lock() {
+                Ok(result) => result,
+                Err(e) => {
+                    panic!("some process error {}",e);
+                }
+            };
+            results.push((i, result));
         });
         handles.push(handle);
     }
-
 
     for handle in handles {
         handle.join().unwrap();
     }
 
-    let mut sorted_results = results.lock().unwrap(); // 結果のVecのロックを取得する
-    sorted_results.sort_by(|a, b| a.0.cmp(&b.0)); // インデックス順に並べる
-    let mut result_mosaic_img:ImageBuffer<Rgba<u8>, Vec<u8>>=ImageBuffer::new(width*50, height*50);
-    for ((y,result)) in sorted_results.iter(){
-        match result_mosaic_img.copy_from(result, 0, height/2*50) {
-            Ok(file)=>file,
-            Err(err)=>panic!("{}", err)
+    let mut sorted_results = results.lock().unwrap();
+    sorted_results.sort_by(|a, b| a.0.cmp(&b.0));
+    let mut result_mosaic_img: ImageBuffer<Rgba<u8>, Vec<u8>> =
+        ImageBuffer::new(width * 50, height * 50);
+    for (_, result) in sorted_results.iter() {
+        match result_mosaic_img.copy_from(result, 0, height / 2 * 50) {
+            Ok(file) => file,
+            Err(err) => panic!("{}", err),
         }
     }
-    result_mosaic_img.save("./result.png").unwrap();
-
+    match result_mosaic_img.save("./result.png") {
+        Ok(_) => {}
+        Err(e) => {
+            panic!("cannot save result mosaic{}", e)
+        }
+    }
 }
-fn calculate_min_color_distance_code(color: Rgb<u8>,image_colors:&HashMap<String,String>)->&str{
-    let mut min_color_distance=196608;
+fn calculate_min_color_distance_code(
+    color: Rgb<u8>,
+    image_colors: &HashMap<String, String>,
+) -> &str {
+    let mut min_color_distance = 196608;
     let mut min_image_key = "0";
-    for (key,image_color) in  image_colors.iter(){
+    for (key, image_color) in image_colors.iter() {
         let image_color_number = hex_to_rgb(image_color);
         let color_distance = calculate_color_distance(image_color_number, color);
-        if color_distance < min_color_distance{
+        if color_distance < min_color_distance {
             min_color_distance = color_distance;
-            min_image_key =key;
+            min_image_key = key;
         }
     }
     min_image_key
-
 }
-fn load_color_code_json()->HashMap<String, String>{
-    let mut file = File::open("./color_code.json").unwrap();
-    // ファイルを文字列として読み込む
+fn load_color_code_json() -> HashMap<String, String> {
+    let mut file = match File::open("./color_code.json") {
+        Ok(file) => file,
+        Err(e) => panic!("cannot open color_code.json {}", e),
+    };
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {}
+        Err(e) => {
+            panic!("cannot load color_code.json {}", e)
+        }
+    }
 
-    // JSONをデシリアライズする
-    let json_data:HashMap<String,String> = serde_json::from_str(&contents).unwrap();
+    let json_data: HashMap<String, String> = match serde_json::from_str(&contents) {
+        Ok(f) => f,
+        Err(e) => {
+            panic!("cannot deserialize color_code.json {}", e);
+        }
+    };
 
-    // HashMapを返す
     json_data
 }
